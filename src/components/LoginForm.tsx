b9af0ae0cac421f4dcc5c130b9e39baa50a15b8a
zafflen528 <abyan@zafflen.xyz>
2023-02-07 10:21:57 +0700

    added login page

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { useFormik } from 'formik'
+import "../styles/LoginForm.css"
 
 const LoginForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password : ''
+    },
+    onSubmit: values => {
+      let userToken = values.email.concat(values.password)
+      localStorage.setItem("token", userToken);
+      console.log(localStorage.getItem("token"));
+      // go to store page
+    }
+  })
   return (
-    <div>LoginForm</div>
+    <form onSubmit={formik.handleSubmit} className="login-form">
+      <input
+        id='email'
+        name='email'
+        type="email"
+        placeholder='EMAIL'
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+      <input
+        id='password'
+        name='password'
+        type="password"
+        placeholder='PASSWORD'
+        onChange={formik.handleChange}
+        value={formik.values.password}
+      />
+      <button type='submit'>Submit</button>
+    </form>
   )
 }
 
